d1a25076ef952d50636d796f0a7696f9292154f5
Kevansh Gopani <130953386+KevanshGopani@users.noreply.github.com>
2023-08-04 10:16:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,28 @@ const ans = () => {
 
 const ans2 = () => {
 	let data = ans()
-	alert(data.charAt(0))
+	
+let index = -1;
+let fnc = ' ';
+
+if(data.length == 0){
+alert("EMPTY STRING");
+}
+
+for (let i of data) {
+	if (data.split(i).length - 1 === 1) {
+		fnc = i;
+		break;
+	} else {
+		index += 1;
+	}
+}
+if (index === data.length-1) {
+console.log("yooo")
+
+	alert("All The Charecter Are reapeating")
+} else {
+	alert(fnc);
+}  
 }
 ans2()
