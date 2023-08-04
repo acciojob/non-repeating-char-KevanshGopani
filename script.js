81cd0eed4537b6910a292555b58c8ca05daa5b1a
Kevansh Gopani <130953386+KevanshGopani@users.noreply.github.com>
2023-08-04 09:37:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+const ans = () => {
+	let value = prompt("Enter String")
+	return value;
+}
+
+const ans2 = () => {
+	let data = ans()
+	alert(data.charAt(0))
+}
+ans2()
